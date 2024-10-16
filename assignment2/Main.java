4711d7885f9cdba4826a8c2123223fef8af5184a
Jacky Guan <guan.yuch@northeastern.edu>
2024-10-15 18:58:25 -0700

    To start the program

---
+++ assignment2/Main.java
@@ -0,0 +1,8 @@
+package assignment2;
+
+public class Main {
+    public static void main(String[] args) {
+        MainJFrame mainFrame = new MainJFrame();
+        mainFrame.setVisible(true);
+    }
+}
