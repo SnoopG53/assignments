b3eda2d2e5e8679f3a44371cc7b9ce526f3afe8c
Jacky Guan <guan.yuch@northeastern.edu>
2024-10-15 17:45:06 -0700

    Update Person and Address class

--- assignment2/Address.java
+++ assignment2/Address.java
@@ -1,14 +1,65 @@
-package assignment2;
-
 public class Address {
-    private String street;
+    private String streetAddress;
+    private String unitNumber;
     private String city;
     private String state;
-    private String zip;
-    public Address(String street, String city, String state, String zip){
-        this.street = street;
+    private String zipCode;
+    private long phoneNumber;
+
+    public Address(String streetAddress, String unitNumber, String city, String state, String zipCode, long phoneNumber) {
+        this.streetAddress = streetAddress;
+        this.unitNumber = unitNumber;
         this.city = city;
         this.state = state;
-        this.zip = zip;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getUnitNumber() {
+        return unitNumber;
+    }
+
+    public void setUnitNumber(String unitNumber) {
+        this.unitNumber = unitNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- assignment2/Person.java
+++ assignment2/Person.java
@@ -1,14 +1,97 @@
-package assignment2;
-
 public class Person {
     private String firstName;
     private String lastName;
+    private String socialSecurityNumber;
+    private String email;
+    private int age;
+    private double height;
+    private double weight;
+    private char gender;
     private Address homeAddress;
     private Address workAddress;
-    public Person(String firstName, String lastName, String homeAddress, String workAddress){
+
+    public Person(String firstName, String lastName, String ssn, String email, int age, double height, double weight, char gender, Address homeAddress, Address workAddress) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.socialSecurityNumber = ssn;
+        this.email = email;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
         this.homeAddress = homeAddress;
         this.workAddress = workAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
